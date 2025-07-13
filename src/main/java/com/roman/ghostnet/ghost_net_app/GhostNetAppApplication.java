7c47741b17ad3fa106fbc6e73dff1410f19b7b86
RomanFritsch <roman.fritsch@web.de>
2025-07-13 16:22:22 +0200

    Add SpringBoot project strucutre

---
+++ src/main/java/com/roman/ghostnet/ghost_net_app/GhostNetAppApplication.java
@@ -0,0 +1,13 @@
+package com.roman.ghostnet.ghost_net_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhostNetAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhostNetAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roman/ghostnet/ghost_net_app/GhostNetAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roman.ghostnet.ghost_net_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhostNetAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
